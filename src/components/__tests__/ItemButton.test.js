a222a45a960d05d0b416c5c2b60d0f4b0a8b18aa
PrainLopez <qym200042@gmail.com>
2023-12-31 03:14:27 +0800

    testing tbd

---
+++ src/components/__tests__/ItemButton.test.js
@@ -0,0 +1,30 @@
+import { mount } from '@vue/test-utils'
+import ItemButtonVue from '../ItemButton.vue'
+
+test('render the image correctly when given image name', async () => {
+  const wrapper = mount(ItemButtonVue, {
+    props: {
+      img: '爆破单元.webp'
+    }
+  })
+  expect(wrapper.find('img').exists()).toBe(true)
+  expect(wrapper.find('img').attributes('src')).toBe('/dsp-icon/爆破单元.webp')
+  expect(wrapper.find('img').attributes('alt')).toBe('爆破单元')
+  expect(wrapper.find('img').attributes('width')).toBe('50px')
+})
+
+test('prop.width', async () => {
+  const wrapper = mount(ItemButtonVue, {
+    props: {
+      img: '爆破单元.webp',
+      width: '100px'
+    }
+  })
+  expect(wrapper.find('img').attributes('width')).toBe('100px')
+})
+
+test('button disabled and img is not rendered when props.img empty', async () => {
+  const wrapper = mount(ItemButtonVue, {})
+  expect(wrapper.find('button:enabled').exists()).toBe('false')
+  expect(wrapper.find('img').exists()).toBe('false')
+})
