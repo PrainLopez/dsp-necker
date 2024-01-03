698858a3e4d25e92b96d3897671ae7ba76917fbe
Prain Lopez <qym200042@gmail.com>
2024-01-03 17:35:26 +0800

    下班

---
+++ src/views/UtilityView.vue
@@ -0,0 +1,15 @@
+<script setup>
+const data = ref('1')
+</script>
+
+<template>
+  <div>
+    <textarea readonly="true" v-model="data">Input grid data</textarea>
+    <button>Convert</button>
+    <div>
+      <pre></pre>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
