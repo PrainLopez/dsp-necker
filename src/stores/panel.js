9e46cfa3134f85895921bfa889681b4e9b6465e9
Prain Lopez <qym200042@gmail.com>
2024-01-05 11:36:47 +0800

    add setters

--- src/stores/panel.js
+++ src/stores/panel.js
@@ -8,4 +8,51 @@ export const usePanelStore = defineStore('panel', () => {
   const facilitySelection = ref('')
   const proliferatorAction = ref('')
   const proliferatorSelection = ref('')
+
+  function setSelectedItem(value) {
+    selectedItem.value = value
+  }
+
+  function setUnitSelection(value) {
+    unitSelection.value = value
+  }
+
+  function setInputAmount(value) {
+    inputAmount.value = value
+  }
+
+  function setFacilityClass(value) {
+    facilityClass.value = value
+  }
+
+  function setFacilitySelection(value) {
+    facilitySelection.value = value
+  }
+
+  function setProliferatorAction(value) {
+    proliferatorAction.value = value
+  }
+
+  function setProliferatorSelection(value) {
+    proliferatorSelection.value = value
+  }
+
+  return {
+    // state
+    selectedItem,
+    unitSelection,
+    inputAmount,
+    facilityClass,
+    facilitySelection,
+    proliferatorAction,
+    proliferatorSelection,
+    // action
+    setSelectedItem,
+    setUnitSelection,
+    setInputAmount,
+    setFacilityClass,
+    setFacilitySelection,
+    setProliferatorAction,
+    setProliferatorSelection
+  }
 })
