c4de458b8aa009dac0d749f2a137c2b728a21025
Prain Lopez <qym200042@gmail.com>
2024-01-31 13:28:59 +0800

    test

--- src/components/__tests__/ProliferatorSwitch.test.js
+++ src/components/__tests__/ProliferatorSwitch.test.js
@@ -7,14 +7,27 @@ test('clicking button should switch text', async () => {
   const wrapper = mount(ProliferatorSwitchVue, {
     props: {
       modelValue: true,
+      options: ['Yes', 'No'],
       'onUpdate:modelValue': (e) =>
         wrapper.setProps({ modelValue: e })
     }
   })
   expect(wrapper.find('span#yield').isVisible()).toBeTruthy()
-  expect(wrapper.find('span#speed').text()).toBeFalsy()
   await wrapper.find('input').setValue(false)
   expect(wrapper.emitted('update:modelValue')).toBeTruthy()
   expect(wrapper.props('modelValue')).toBe(false)
   expect(wrapper.find('span#yield').isVisible()).toBeTruthy()
+  // fixme: 2 text always visible
+})
+
+test('disable input when props.disabled is true', async () => {
+  const wrapper = mount(ProliferatorSwitchVue, {
+    props: {
+      modelValue: true,
+      options: [],
+      'onUpdate:modelValue': (e) =>
+        wrapper.setProps({ modelValue: e })
+    }
+  })
+  expect(wrapper.find('input').attributes('disabled')).toBeTruthy()
 })
